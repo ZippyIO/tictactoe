dd2bce350abc3f0b3df791f72bf72826cc65bfc8
Nick <ZippyIO@users.noreply.github.com>
2022-11-22 14:42:17 +1100

    added player switching with keydown changes players marker and displays on dom updates position in board array with players marker

--- scripts/script.js
+++ scripts/script.js
@@ -10,20 +10,55 @@ const displayController = (() => {
         boardLocation.className = 'board-tile';
         boardLocation.setAttribute('data-tile', `${location}`);
         boardDiv.appendChild(boardLocation);
-        boardLocation.addEventListener('click', () => {
-            console.log(`clicked tile ${location}`);
-            updateTile(location);
-        });
+        boardLocation.addEventListener(
+            'click',
+            () => {
+                updateTile(location);
+            },
+            { once: true },
+        );
     });
-    const updateTile = (tile, player) => {
+    const updateTile = (tile) => {
         const playedTile = document.querySelector(`[data-tile="${tile}"]`);
         const tileSelected = document.createElement('p');
         tileSelected.classList = 'played-tile';
-        tileSelected.textContent = 'X';
+        if (game.playerID == 1) {
+            tileSelected.textContent = 'X';
+            playedTile.setAttribute('data-tile', 'X');
+            gameBoard.board.splice(tile - 1, 1, 'X');
+        } else if (game.playerID == 2) {
+            tileSelected.textContent = 'O';
+            playedTile.setAttribute('data-tile', 'O');
+            gameBoard.board.splice(tile - 1, 1, 'O');
+        }
         playedTile.appendChild(tileSelected);
+        game.checkWin(gameBoard.board);
     };
 
     return { addTiles, updateTile };
 })();
 
-const Player = (id) => {};
+const Player = () => {
+    const changePlayer = () => {
+        addEventListener('keydown', (event) => {
+            if (event.key == 1 || event.key == 2) {
+                game.playerID = parseInt(event.key);
+            }
+        });
+    };
+
+    return { changePlayer };
+};
+
+const game = (() => {
+    let playerID;
+    const player = Player();
+    //player.changePlayer();
+    const setPlayer = () => {
+        playerID = 1;
+        player.changePlayer();
+    };
+    setPlayer();
+    const checkWin = (board) => {};
+    return { playerID, checkWin };
+})();
