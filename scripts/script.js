e76e26b5d27db07ac78e003c945e9f29f4b370e1
Nick <ZippyIO@users.noreply.github.com>
2022-11-22 15:07:42 +1100

    added game logic that detects when a marker has three in a row and disables input on the game board

--- scripts/script.js
+++ scripts/script.js
@@ -19,21 +19,29 @@ const displayController = (() => {
         );
     });
     const updateTile = (tile) => {
-        const playedTile = document.querySelector(`[data-tile="${tile}"]`);
-        const tileSelected = document.createElement('p');
-        tileSelected.classList = 'played-tile';
-        if (game.playerID == 1) {
-            tileSelected.textContent = 'X';
-            playedTile.setAttribute('data-tile', 'X');
-            gameBoard.board.splice(tile - 1, 1, 'X');
-        } else if (game.playerID == 2) {
-            tileSelected.textContent = 'O';
-            playedTile.setAttribute('data-tile', 'O');
-            gameBoard.board.splice(tile - 1, 1, 'O');
+        if (!game.gameOver) {
+            const playedTile = document.querySelector(`[data-tile="${tile}"]`);
+            const tileSelected = document.createElement('p');
+            tileSelected.classList = 'played-tile';
+            if (game.playerID == 1) {
+                tileSelected.textContent = 'X';
+                playedTile.setAttribute('data-tile', 'X');
+                gameBoard.board.splice(tile - 1, 1, 'X');
+            } else if (game.playerID == 2) {
+                tileSelected.textContent = 'O';
+                playedTile.setAttribute('data-tile', 'O');
+                gameBoard.board.splice(tile - 1, 1, 'O');
+            }
+            playedTile.appendChild(tileSelected);
+            game.checkWin(gameBoard.board);
+        } else if (game.gameOver) {
+            boardDiv.removeEventListener('click', () => {
+                disableTiles();
+            });
         }
-        playedTile.appendChild(tileSelected);
-        game.checkWin(gameBoard.board);
     };
+
+    const disableTiles = () => {};
 
     return { addTiles, updateTile };
 })();
@@ -52,6 +60,7 @@ const Player = () => {
 
 const game = (() => {
     let playerID;
+    let gameOver = false;
     const player = Player();
     //player.changePlayer();
     const setPlayer = () => {
@@ -59,6 +68,32 @@ const game = (() => {
         player.changePlayer();
     };
     setPlayer();
-    const checkWin = (board) => {};
-    return { playerID, checkWin };
+    const checkWin = (board) => {
+        if (
+            (board[0] == 'X' && board[1] == 'X' && board[2] == 'X') ||
+            (board[3] == 'X' && board[4] == 'X' && board[5] == 'X') ||
+            (board[6] == 'X' && board[7] == 'X' && board[8] == 'X') ||
+            (board[0] == 'X' && board[3] == 'X' && board[6] == 'X') ||
+            (board[1] == 'X' && board[4] == 'X' && board[7] == 'X') ||
+            (board[2] == 'X' && board[5] == 'X' && board[8] == 'X') ||
+            (board[0] == 'X' && board[4] == 'X' && board[8] == 'X') ||
+            (board[2] == 'X' && board[4] == 'X' && board[6] == 'X')
+        ) {
+            console.log('X');
+            game.gameOver = true;
+        } else if (
+            (board[0] == 'O' && board[1] == 'O' && board[2] == 'O') ||
+            (board[3] == 'O' && board[4] == 'O' && board[5] == 'O') ||
+            (board[6] == 'O' && board[7] == 'O' && board[8] == 'O') ||
+            (board[0] == 'O' && board[3] == 'O' && board[6] == 'O') ||
+            (board[1] == 'O' && board[4] == 'O' && board[7] == 'O') ||
+            (board[2] == 'O' && board[5] == 'O' && board[8] == 'O') ||
+            (board[0] == 'O' && board[4] == 'O' && board[8] == 'O') ||
+            (board[2] == 'O' && board[4] == 'O' && board[6] == 'O')
+        ) {
+            console.log('O');
+            game.gameOver = true;
+        }
+    };
+    return { playerID, gameOver, checkWin };
 })();
