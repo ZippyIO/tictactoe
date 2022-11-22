802ee8631f05d9628d9258a965aff9d06908dab6
Nick <ZippyIO@users.noreply.github.com>
2022-11-22 16:24:16 +1100

    added working score counter and game restarter

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,39 @@
 const gameBoard = (() => {
     let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    return { board };
+    const setEmptyBoard = () => {
+        gameBoard.board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    };
+    return { board, setEmptyBoard };
 })();
 
 const displayController = (() => {
     const boardDiv = document.querySelector('#board');
-    const addTiles = gameBoard.board.forEach((location) => {
-        const boardLocation = document.createElement('div');
-        boardLocation.className = 'board-tile';
-        boardLocation.setAttribute('data-tile', `${location}`);
-        boardDiv.appendChild(boardLocation);
-        boardLocation.addEventListener(
-            'click',
-            () => {
-                updateTile(location);
-            },
-            { once: true },
-        );
-    });
+    const addTiles = () => {
+        gameBoard.board.forEach((location) => {
+            const boardLocation = document.createElement('div');
+            boardLocation.className = 'board-tile';
+            boardLocation.setAttribute('data-tile', `${location}`);
+            boardDiv.appendChild(boardLocation);
+            boardLocation.addEventListener(
+                'click',
+                () => {
+                    updateTile(location);
+                },
+                { once: true },
+            );
+        });
+    };
+
     const updateTile = (tile) => {
         if (!game.gameOver) {
             const playedTile = document.querySelector(`[data-tile="${tile}"]`);
             const tileSelected = document.createElement('p');
             tileSelected.classList = 'played-tile';
-            if (game.playerID == 1) {
+            if (player.playerID == 1) {
                 tileSelected.textContent = 'X';
                 playedTile.setAttribute('data-tile', 'X');
                 gameBoard.board.splice(tile - 1, 1, 'X');
-            } else if (game.playerID == 2) {
+            } else if (player.playerID == 2) {
                 tileSelected.textContent = 'O';
                 playedTile.setAttribute('data-tile', 'O');
                 gameBoard.board.splice(tile - 1, 1, 'O');
@@ -41,45 +47,65 @@ const displayController = (() => {
         }
     };
 
-    const changePlayerUI = () => {
-        if (event.key == 1 || event.key == 2) {
-            game.playerID = parseInt(event.key);
-            if (game.playerID == 1) {
+    const removeTiles = () => {
+        if (game.gameOver) {
+            while (boardDiv.firstChild) {
+                boardDiv.removeChild(boardDiv.firstChild);
+            }
+            gameBoard.setEmptyBoard();
+            addTiles();
+            game.gameOver = false;
+        }
+    };
+
+    const updatePlayerUI = (e) => {
+        playerOnePara = document.querySelector('.player-one');
+        playerTwoPara = document.querySelector('.player-two');
+        if (e.key == 1 || e.key == 2) {
+            player.playerID = parseInt(e.key);
+            if (player.playerID == 1) {
                 playerOnePara.classList.add('current-player');
                 playerTwoPara.classList.remove('current-player');
-            } else if (game.playerID == 2) {
+            } else if (player.playerID == 2) {
                 playerTwoPara.classList.add('current-player');
                 playerOnePara.classList.remove('current-player');
             }
         }
     };
 
-    return { addTiles, updateTile, changePlayerUI };
+    const updateScoreUI = () => {
+        gameScorePara = document.querySelector('.game-stats');
+        gameScorePara.textContent = `X: ${game.playerOneScore} - O: ${game.playerTwoScore}`;
+    };
+
+    return { addTiles, updateTile, removeTiles, updatePlayerUI, updateScoreUI };
 })();
 
-const Player = () => {
+const player = (() => {
+    let playerID;
     const changePlayer = () => {
-        playerOnePara = document.querySelector('.player-one');
-        playerTwoPara = document.querySelector('.player-two');
         addEventListener('keydown', (event) => {
-            displayController.changePlayerUI();
+            displayController.updatePlayerUI(event);
         });
     };
 
-    return { changePlayer };
-};
+    return { playerID, changePlayer };
+})();
 
 const game = (() => {
-    let playerID;
+    let playerOneScore;
+    let playerTwoScore;
     let gameOver = false;
-    const player = Player();
-    //player.changePlayer();
+    displayController.addTiles();
     const setPlayer = () => {
-        playerID = 1;
+        player.playerID = 1;
+        playerOneScore = 0;
+        playerTwoScore = 0;
         player.changePlayer();
     };
     setPlayer();
     const checkWin = (board) => {
+        let winner;
         if (
             (board[0] == 'X' && board[1] == 'X' && board[2] == 'X') ||
             (board[3] == 'X' && board[4] == 'X' && board[5] == 'X') ||
@@ -90,8 +116,9 @@ const game = (() => {
             (board[0] == 'X' && board[4] == 'X' && board[8] == 'X') ||
             (board[2] == 'X' && board[4] == 'X' && board[6] == 'X')
         ) {
-            console.log('X');
-            game.gameOver = true;
+            winner = 'X';
+            console.log(`winner: ${winner}`);
+            endGame(winner);
         } else if (
             (board[0] == 'O' && board[1] == 'O' && board[2] == 'O') ||
             (board[3] == 'O' && board[4] == 'O' && board[5] == 'O') ||
@@ -102,9 +129,24 @@ const game = (() => {
             (board[0] == 'O' && board[4] == 'O' && board[8] == 'O') ||
             (board[2] == 'O' && board[4] == 'O' && board[6] == 'O')
         ) {
-            console.log('O');
+            winner = 'O';
+            endGame(winner);
+        }
+    };
+
+    const endGame = (winner) => {
+        if (winner == 'X') {
+            game.playerOneScore += 1;
+            displayController.updateScoreUI();
+            game.gameOver = true;
+        } else if (winner == 'O') {
+            game.playerTwoScore += 1;
+            displayController.updateScoreUI();
             game.gameOver = true;
         }
     };
-    return { playerID, gameOver, checkWin };
+
+    const nextGame = () => {};
+
+    return { playerOneScore, playerTwoScore, gameOver, checkWin };
 })();
